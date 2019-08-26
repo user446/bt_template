36c71f7d3d6ff18dfc154411b4dd136ec79d3402
user446 <53828704+user446@users.noreply.github.com>
2019-08-26 12:42:43 +0300

    Изменение python_graph
    + PyQt5, pyqtgraph
    + реалтайм

--- python_graph/run.py
+++ python_graph/run.py
@@ -1,66 +1,101 @@
+from PyQt5 import QtCore, QtWidgets
+import pyqtgraph as pg
 import serial
 import io
 import numpy as np
-import matplotlib as mtp
-import matplotlib.pyplot as pt
-import matplotlib.animation as anim
 
+# инициализация COM порта
 ser = serial.Serial(port="COM7", baudrate=115200,
                     bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_NONE)
 
-fig = pt.figure()
-ax = pt.axes(xlim = [0,1024], ylim = [-3000,3000])
-line, = ax.plot([], [])
-xdata = []
-ydata = []
+# открытие файла логов и его очистка
 f = open("logs.txt", "w+")
 f.truncate()
 t = 0
+x = []
+y = []
 
 
-def run(data):
-    t,y = data
-    xdata.append(t)
-    ydata.append(y)
-    line.set_data(xdata, ydata)
-    return line,
+class MyWidget(pg.GraphicsWindow):
 
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
 
-def get_data():
-    t_list = [0]*4
-    i = 0
-    global t
-    while i < 4:
-        t_list[i] =  t
-        t += 4
-        i += 1
-    ser_data = ser.read_until(terminator=serial.LF).decode('utf-8')
-    data = ser_data.split("::")
-    if len(data) < 3:
-        return
-    f.write(ser_data)
-    
-    new_list = list(filter(None, data[1].split(" ")))
-    if len(new_list) < 4:
-        return
-    dt = np.array(new_list).astype(np.float)
-    
-    print(t_list, dt)
-    yield t_list, dt
+        self.mainLayout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.mainLayout)
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(10)  # in milliseconds
+        self.timer.start()
+        self.timer.timeout.connect(self.onNewData)
+
+        self.plotItem = self.addPlot(title="BlueCardio Output")
+        self.plotItem.setYRange(0, 100, padding=0)
+
+        self.plotDataItem = self.plotItem.plot([], pen=pg.mkPen('b', width=5),
+                                               symbolBrush=(255, 0, 0), symbolSize=5, symbolPen=None)
+
+    def setData(self, x, y):
+        self.plotDataItem.setData(x, y)
+
+    def onNewData(self):
+        ser_data = ser.read_until(terminator=serial.LF).decode(
+            'utf-8')  # считываем сообщение из порта до конца
+        data = ser_data.split("::")  # разделяем на составляющие
+        if len(data) < 3:  # если сообщение не было принято целиком, то
+            return  # сбрасываем
+        f.write(ser_data)  # записываем сообщение в файл
+        print(ser_data)
+        # разедяем числа по пробелам, выкидываем пустые элементы листа
+        new_list = list(filter(None, data[1].split(" ")))
+        if len(new_list) != 4:  # если принято не 4 числа, то что-то не так
+            return
+        
+        t_list = [0]*4
+        i = 0
+        global t
+        while i < 4:
+            t_list[i] = t
+            t += 4
+            i += 1
+
+        global x
+        global y
+        try:
+            x = np.append(x, np.array(t_list).astype(np.int))
+        except ValueError:
+            return
+        # преобразуем лист в float и записываем его в данные оси y
+        y = np.append(y, np.array(new_list).astype(np.float))
+
+        if len(x) > 512:
+            x = np.delete(x, [0,1,2,3])
+            y = np.delete(y, [0,1,2,3])
+        self.setData(x, y)
 
 
 def main():
     print("Starting BlueCardio graph application")
     print("Trying to open port...")
 
+    # попытка открыть порт
     try:
         ser.open()
-    except IOError:
+    except IOError:  # скорее всего уже открыт, а значит переоткрываем
         ser.close()
         ser.open()
-    
-    ani = anim.FuncAnimation(fig, run, get_data, interval=50, blit=False)
-    pt.show()
+
+    app = QtWidgets.QApplication([])
+
+    pg.setConfigOptions(antialias=False)  # True seems to work as well
+    pg.setConfigOption('background', 'w')
+    pg.setConfigOption('foreground', 'k')
+
+    win = MyWidget()
+    win.show()
+    win.resize(800, 600)
+    win.raise_()
+    app.exec_()
 
 
 if __name__ == "__main__":
